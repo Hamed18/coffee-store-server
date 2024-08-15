99857c4a9aecfaf61a396da87cb36205dde84842
hamed18 <hamedcnec@gmail.com>
2024-08-16 03:52:42 +0600

    client-server connection error solved. send user data from server to mongodb database

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
@@ -13,7 +13,7 @@ app.use(express.json());
 console.log(process.env.DB_USER)   // https://www.npmjs.com/package/dotenv
 console.log(process.env.DB_PASS)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3nkfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3nkfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,12 +28,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+	const coffeeCollection = client.db("coffeeDB").collection("coffee"); // server to database
+    
+	app.post('/coffee', async(req,res) => {
+		const newCoffee = req.body;
+		console.log(newCoffee);
+		const result = await coffeeCollection.insertOne(newCoffee);
+		res.send(result);
+	})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+ //   await client.close();
   }
 }
 run().catch(console.dir);
